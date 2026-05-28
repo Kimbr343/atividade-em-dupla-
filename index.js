d77ad795baacec5d2ee382ee581e8939f7da5dc6
Kimbr343 <jmf9@discente.ifpe.edu.br>
2026-05-28 11:53:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Bem vindo ao Sistema`');
 });
 
+app.get('/sobre', (req, res) => {
+  res.send('Sistema sobre o Santa Cruz')
+})
+
 app.listen(
     3000, 
     () => console.log('sistema encontrado')
